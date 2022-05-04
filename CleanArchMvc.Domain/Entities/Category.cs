dca5c884f22c61e5a63ee6e6f91546cf8411ad6f
Robson Silva <rms2307@outlook.com>
2022-05-04 17:47:10 -0300

    Update: Domain

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : BaseEntity
+    {
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required!");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Minimum 3 charecters");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int? id);
+
+        Task<Product> GetProductByCategoryAsync(int? categoryId);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
