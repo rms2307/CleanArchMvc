030889f6a236c2021d9bb4742dbc411ef3609b43
Robson Silva <rms2307@outlook.com>
2022-05-06 11:36:31 -0300

    Add: Controller de teste para categorias

---
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _service;
+
+        public CategoriesController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _service.GetCategoriesAsync();
+            return View(categories);
+        }
+    }
+}
