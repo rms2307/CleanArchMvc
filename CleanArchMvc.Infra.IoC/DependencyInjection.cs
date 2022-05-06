c1b7464b08cfde760b1bdc000575dcd30a1ba1c9
Robson Silva <rms2307@outlook.com>
2022-05-06 11:20:36 -0300

    Update: registrando serviços

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using CleanArchMvc.Domain.Interfaces;
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -11,14 +14,39 @@ namespace CleanArchMvc.Infra.IoC
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            RegisterContext(services, configuration);
+
+            RegisterRepositories(services, configuration);
+
+            RegisterServices(services, configuration);
+
+            RegisterAutoMapper(services, configuration);
+
+            return services;
+        }
+
+        private static void RegisterAutoMapper(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+        }
+
+        private static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+        }
+
+        private static void RegisterRepositories(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+
+        private static void RegisterContext(IServiceCollection services, IConfiguration configuration)
+        {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-
-            return services;
         }
     }
 }
