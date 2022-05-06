fdec32b60f2f9567fd9036d6edcbae183022135a
Robson Silva <rms2307@outlook.com>
2022-05-06 11:51:20 -0300

    Add: Controller de teste para produtos

---
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _service.GetProductsAsync();
+            return View(products);
+        }
+    }
+}
