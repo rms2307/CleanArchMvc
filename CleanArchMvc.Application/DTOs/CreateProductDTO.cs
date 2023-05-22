e6a0590b3aa9d2381bbba28985ac212714c05428
Robson de Moraes Silva <rms2307@outlook.com>
2023-05-21 23:57:21 -0300

    Usando FluentValidation para validar Dto de entrada

--- CleanArchMvc.Application/DTOs/CreateProductDTO.cs
+++ CleanArchMvc.Application/DTOs/CreateProductDTO.cs
@@ -1,41 +1,15 @@
-﻿using CleanArchMvc.Application.Annotations;
-using Microsoft.AspNetCore.Http;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace CleanArchMvc.Application.DTOs
 {
     public class CreateProductDTO
     {
-        public int Id { get; set; }
-
-        [Required(ErrorMessage = "The Name is Required")]
-        [MinLength(3)]
-        [MaxLength(100)]
         public string Name { get; set; }
-
-        [Required(ErrorMessage = "The Description is Required")]
-        [MinLength(5)]
-        [MaxLength(200)]
         public string Description { get; set; }
-
-        [Required(ErrorMessage = "The Price is Required")]
-        [Column(TypeName = "decimal(18,2)")]
-        [DisplayFormat(DataFormatString = "{0:C2}")]
-        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
-
-        [Required(ErrorMessage = "The Stock is Required")]
-        [Range(1, 9999)]
         public int Stock { get; set; }
-
-        [FileSizeValidation(1024 * 1024, ErrorMessage = "Image size should be less than 1 MB.")]
-        [FileExtensionsValidation(new string[] { ".jpg", ".jpeg", ".png" }, ErrorMessage = "Only .jpg, .jpeg and .png files are allowed.")]
         public IFormFile Image { get; set; }
-
         public string ImageUrl { get; set; }
-
         public int CategoryId { get; set; }
     }
 }
---
+++ CleanArchMvc.Application/DTOs/Validations/CreateProductValidator.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Linq;
+
+namespace CleanArchMvc.Application.DTOs.Validations
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDTO>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .MinimumLength(3)
+                .MaximumLength(100)
+                .NotEmpty();
+
+            RuleFor(x => x.Description)
+                .MinimumLength(5)
+                .MaximumLength(200)
+                .NotEmpty();
+
+            RuleFor(x => x.Price)
+                .PrecisionScale(18, 2, true)
+                .NotNull();
+
+            RuleFor(x => x.Stock)   
+                .InclusiveBetween(1, 9999)
+                .NotNull();
+
+            RuleFor(x => x.Image).Custom((image, context) =>
+            {
+                ValidateFileSize(image, context);
+            });
+
+            RuleFor(x => x.Image).Custom((image, context) =>
+            {
+                ValidateFileExtensions(image, context);
+            });
+        }
+
+        private static void ValidateFileSize(IFormFile image, ValidationContext<CreateProductDTO> context)
+        {
+            int maxImageSize = 1024 * 1024;
+            if (image != null && image.Length > maxImageSize)
+                context.AddFailure("Image size should be less than 1 MB.");
+        }
+
+        private static void ValidateFileExtensions(IFormFile image, ValidationContext<CreateProductDTO> context)
+        {
+            string[] allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (image is IFormFile file)
+            {
+                var fileExtension = Path.GetExtension(file.FileName);
+
+                if (!allowedExtensions.Contains(fileExtension.ToLower()))
+                    context.AddFailure($"Only {allowedExtensions} files are allowed.");
+            }
+        }
+    }
+}
